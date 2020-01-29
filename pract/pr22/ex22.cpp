ec7141dbac344020764b027074f9505dc3d7212b
ak1light <55546923+ak1light@users.noreply.github.com>
2020-01-29 04:33:15 +0300

    Add files via upload

---
+++ pract/pr22/ex22.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+void swap(int*, int*);
+int msort(int*, int*, int*);
+void info(const int*);
+int* add(int*, const int*);
+
+		int main() {
+			int a = 5, b = 3, c = 1;
+			info(&a);
+			info(&b);
+			info(&c);
+			msort(&a, &b, &c);
+			info(add(&a, add(&b, &c))); 
+			
+			return(0);
+		}
+		void swap(int* a, int* b) {
+			*a = *a + *b;
+			*b = *a - *b;
+			*a = *a - *b;	
+		}
+		void info(const int* a) {
+			std::cout << "Address:" << a << ";" << "Zna4enie:" << *a << ";"<< std::endl;
+		}
+		int msort(int* a , int* b , int* c) {
+			if (*a > * b) swap(a, b);
+			if (*b > * c) swap(b, c);
+			if (*a > * b) swap(a, b);
+			if (*b > * c) swap(b, c);
+			
+			return (*c);
+		}
+		int* add(int* a, const int* c) {
+			*a += *c;
+			return(a);
+		}
